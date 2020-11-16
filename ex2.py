b288ab2677d9560d8624869172c0e15e508b765f
Daalin80 <73665109+Daalin80@users.noreply.github.com>
2020-11-16 14:45:30 +0200

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,59 @@
+list_to_keep = []
+while True:
+    number = int(input("Введите число: "))
+    if number == 0:
+        break
+    else:
+        list_to_keep.append(number)
+print("Выводим введённые числа: ", list_to_keep)
+
+print("Количество введённых чисел: ", len(list_to_keep))
+
+def summa(list_to_keep):
+    thesumma = 0
+    for i in list_to_keep:
+        thesumma += i
+    return thesumma
+print("Сумма введённых чисел: ", summa(list_to_keep))
+
+def multiply(list_to_keep):
+    themulti = 1
+    for i in list_to_keep:
+        themulti *= i
+    return themulti
+print("Произведение введённых чисел: ", multiply(list_to_keep))
+
+def average(list_to_keep):
+    return sum(list_to_keep) / len(list_to_keep)
+print("Среднее арифметическое введённых чисел: ", average(list_to_keep))
+
+def max(list_to_keep):
+    index_of_max = 0
+    for i in list_to_keep:
+        if i > index_of_max:
+            index_of_max = i
+    return(index_of_max)
+print("Выводим значение и порядковый номер наибольшего элемента последовательности: ",
+max(list_to_keep),   list_to_keep.index(max(list_to_keep)) + 1)
+
+def evenodd(list_to_keep):
+    even = 0
+    odd = 0
+    for i in list_to_keep:
+        if i % 2 == 0:
+            even += 1
+        else:
+            odd += 1
+    return even, odd
+print("Выводим количество чётных и нечётных элементов в последовательности: ", evenodd(list_to_keep))
+
+def secondmax(list_to_keep):
+    max_digit = 0
+    second_max_digit = 0
+    for i, value in enumerate(list_to_keep):
+        if value > max_digit:
+            max_digit = value
+        if value < max_digit:
+            second_max_digit = value
+    return(second_max_digit)
+print(secondmax(list_to_keep))
